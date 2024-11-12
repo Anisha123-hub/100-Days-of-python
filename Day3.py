6da574360f3e8a96d11866d8d3602b02ec74bc28
Anisha123-hub <84566081+Anisha123-hub@users.noreply.github.com>
2024-11-12 17:45:12 +0545

    Add files via upload
    # Day 5 - Comments, Escape sequence & Print in Python
    
    Welcome to Day 5 of 100DaysOfCode. Today we will talk about Comments, Escape Sequences and little bit more about print statement in Python.
    We will also throw some light on Escape Sequences
    
    # Python Comments
    A comment is a part of the coding file that the programmer does not want to execute, rather the programmer uses it to either explain a block of code or to avoid the execution of a specific part of code while testing.
    
    ## Single-Line Comments:
    
    To write a comment just add a ‘#’ at the start of the line.
    
    ### Example 1
    
    ```python
    #This is a 'Single-Line Comment'
    print("This is a print statement.")
    ``` 
    
    Output:
    
    ```markup
    This is a print statement. 
    ``` 
    
    ### Example 2
    
    ```python
    print("Hello World !!!") #Printing Hello World
    ```
    
    Output:
    
    ```markup
    Hello World !!!
    ``` 
    
    ### Example 3:
    
    ```python
    print("Python Program")
    #print("Python Program")
    ``` 
    
    ### Output: 
    
    ```markup
    Python Program
    ``` 
    ## Multi-Line Comments:
    
    To write multi-line comments you can use ‘#’ at each line or you can use the multiline string.
    
    **Example 1:** The use of ‘#’.
    
    ```python
    #It will execute a block of code if a specified condition is true.
    #If the condition is false then it will execute another block of code.
    p = 7
    if (p > 5):
        print("p is greater than 5.")
    else:
        print("p is not greater than 5.")
    ```
    
    
    Output:
    
    ```markup
    p is greater than 5.
    ```
    
    
    **Example 2:** The use of multiline string.
    
    ```python
    """This is an if-else statement.
    It will execute a block of code if a specified condition is true.
    If the condition is false then it will execute another block of code."""
    p = 7
    if (p > 5):
        print("p is greater than 5.")
    else:
        print("p is not greater than 5.")
    ```
    
    
    ### Output
    
    ```markup
    p is greater than 5.
    ```
    
    # Escape Sequence Characters
    
    To insert characters that cannot be directly used in a string, we use an escape sequence character.
    
    An escape sequence character is a backslash  `\`  followed by the character you want to insert.
    
    An example of a character that cannot be directly used in a string is a double quote inside a string that is surrounded by double quotes:
    
    ```python
    print("This doesnt "execute")
    print("This will \" execute")
    ```

---
+++ Day3.py
@@ -0,0 +1,13 @@
+# This is a comment section
+#print("This section is commented!!!!")
+# This is single line code
+print("Learned how to add a comment line in python")
+print(" we can comment/ uncomment a line using a short cut key using ctrl+/ ")
+
+'''
+This is multilined comment
+
+'''
+
+# Escape Sequence character
+print("Hello my name is \"Anisha Paudel.\" \n I am  currently studying Bachelors.")
