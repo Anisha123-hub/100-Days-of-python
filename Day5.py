e824cbb6b437fd26521bc333ce7e9737f1ff526c
Anisha123-hub <84566081+Anisha123-hub@users.noreply.github.com>
2024-11-14 21:03:49 +0545

    Add files via upload
    # Operators
    Python has different types of operators for different operations. To create a calculator we require arithmetic operators.
    # Arithmetic operators
    
    
    |   Operator             |Operator Name                          |Example                         |
    |----------------|-------------------------------|-----------------------------|
    |+|`Addition`            |``` 15+7 ```            |
    |-|`Subtraction`            |``` 15-7 ```            |
    |*|`Multiplication`            |``` 5*7 ```            |
    |**|`Exponential`            |``` 5**3 ```            |
    |/|`Division`            |``` 5/3 ```            |
    |%|`Modulus`            |``` 15%7 ```            |
    |//|`Floor Division`            |``` 15//7 ```            |

---
+++ Day5.py
@@ -0,0 +1,24 @@
+# lets assign value to any two variables and use it to perform arithmetic operations
+a = 10
+b = 4
+
+# Addition
+print(a + b)
+
+# Substraction
+print(a -b)
+
+# Multiplication
+print(a * b)
+
+# Division
+print(a / b)
+
+# Exponential
+print(a ** b)
+
+# Modulus
+print(a % b)
+
+# Floor Division
+print(a // b)
