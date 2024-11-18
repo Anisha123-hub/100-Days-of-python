f95a679f7e575eb452ac505b143324f84aea32f2
Anisha123-hub <84566081+Anisha123-hub@users.noreply.github.com>
2024-11-18 16:06:35 +0545

    Add files via upload
    # What are strings?
    In python, anything that you enclose between single or double quotation marks is considered a string. A string is essentially a sequence or array of textual data. Strings are used when working with Unicode characters. 
    ## Example
    ```python
    name = "Anisha"
    print("Hello, " + name)
    ```
    ## Output
    Hello, Anisha
    
    Note: It does not matter whether you enclose your strings in single or double quotes, the output remains the same. 
    
    Sometimes, the user might need to put quotation marks in between the strings. Example, consider the sentence: He said, “I want to eat an apple”.
    
    How will you print this statement in python?: 
    ```He said, "I want to eat an apple".```
    We will definitely use single quotes for our convenience
    ```python
    print('He said, "I want to eat an apple".')
    ```
    
    ## Multiline Strings
    If our string has multiple lines, we can create them like this: 
    ```python 
    a = """Lorem ipsum dolor sit amet,
    consectetur adipiscing elit,
    sed do eiusmod tempor incididunt
    ut labore et dolore magna aliqua."""
    print(a)
    ```
    
    ## Accessing Characters of a String
    In Python, string is like an array of characters. We can access parts of string by using its index which starts from 0.\
    Square brackets can be used to access elements of the string.
    
    ```python 
    print(name[0])
    print(name[1])
    
    ```
    
    ## Looping through the string
    We can loop through strings using a for loop like this: 
    ```python
    for character in name:
        print(character)
    ```

---
+++ Day8.py
@@ -0,0 +1,26 @@
+name = "Anisha Paudel"
+
+sentence = 'He said,"I want to eat an apple"'
+# OR we can use escape sequence
+# sentence = "He said,\"I want to eat an apple""
+
+print(sentence)
+
+new = """ Hello, My name is Anisha Paudel
+I love playing table tennis."""
+
+print(new)
+
+
+print("Hello, " + name)
+
+# printing array of characters
+
+print(sentence[0])
+print(sentence[1])
+print(sentence[2])
+print(sentence[3])
+
+# using for for printing all characters 
+for character in name:
+    print(character)
